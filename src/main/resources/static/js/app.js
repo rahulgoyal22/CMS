18a0b68c418803a46ab6bb159dbf6a67a3bd85d9
oyo <oyo@Anshika.domain.name>
2020-09-10 23:39:32 +0530

    ui

--- src/main/resources/static/js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-var demoApp = angular.module('cash.management.system', ['ui.bootstrap', 'cash.management.system.controllers',
-    'cash.management.system.services'
-]);
-demoApp.constant("CONSTANTS", {
-    getUserByIdUrl: "/users/{id}",
-    getAllUsers: "/users/",
-    saveUser: "/users/"
-});
