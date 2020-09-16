4c11f3116a794f76b39282b6a1811645ad9dc3ae
Ashish <ashish.prajapati@oyorooms.com>
2020-09-16 13:49:33 +0530

    inputfile

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -10,19 +10,24 @@ INSERT INTO public.users (id, created_at, updated_at, email, password, name_of_u
 VALUES (5, '2020-09-15', '2020-09-15', 'collector3', 'testing', 'collector3', 0);
 
 INSERT INTO public.hotel (id, created_at, updated_at, email_address, password, manager_id, users_id, balance)
-VALUES (6, '2020-09-15', '2020-09-15', 'hotel1', 'testing', 'Hotel1', '3', 0);
+VALUES (1, '2020-09-15', '2020-09-15', 'hotel1', 'testing', 'Hotel1', '3', 0);
 INSERT INTO public.hotel (id, created_at, updated_at, email_address, password, manager_id, users_id, balance)
-VALUES (7, '2020-09-15', '2020-09-15', 'hotel2', 'testing', 'Hotel2', '3', 0);
+VALUES (2, '2020-09-15', '2020-09-15', 'hotel2', 'testing', 'Hotel2', '3', 0);
 INSERT INTO public.hotel (id, created_at, updated_at, email_address, password, manager_id, users_id, balance)
-VALUES (8, '2020-09-15', '2020-09-15', 'hotel3', 'testing', 'Hotel3', '4', 0);
+VALUES (3, '2020-09-15', '2020-09-15', 'hotel3', 'testing', 'Hotel3', '4', 0);
 INSERT INTO public.hotel (id, created_at, updated_at, email_address, password, manager_id, users_id, balance)
-VALUES (9, '2020-09-15', '2020-09-15', 'hotel4', 'testing', 'Hotel4', '4', 0);
+VALUES (4, '2020-09-15', '2020-09-15', 'hotel4', 'testing', 'Hotel4', '4', 0);
 INSERT INTO public.hotel (id, created_at, updated_at, email_address, password, manager_id, users_id, balance)
-VALUES (10, '2020-09-15', '2020-09-15', 'hotel5', 'testing', 'Hotel5', '5', 0);
+VALUES (5, '2020-09-15', '2020-09-15', 'hotel5', 'testing', 'Hotel5', '5', 0);
 
 INSERT INTO public.mapping (id, created_at, updated_at, collector_id, approver_id)
-VALUES (11, '2020-09-15', '2020-09-15', 3, 1);
+VALUES (1, '2020-09-15', '2020-09-15', 3, 1);
 INSERT INTO public.mapping (id, created_at, updated_at, collector_id, approver_id)
-VALUES (12, '2020-09-15', '2020-09-15', 4, 1);
+VALUES (2, '2020-09-15', '2020-09-15', 4, 1);
 INSERT INTO public.mapping (id, created_at, updated_at, collector_id, approver_id)
-VALUES (13, '2020-09-15', '2020-09-15', 5, 2);
+VALUES (3, '2020-09-15', '2020-09-15', 5, 2);
+
+SELECT setval('users_id_seq',5, true);
+SELECT setval('hotel_id_seq',5, true);
+SELECT setval('mapping_id_seq',3, true);
+
